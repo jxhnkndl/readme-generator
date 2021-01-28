4e7f80d239124a15ce4c2b4ddb8dd6969aa703a2
jxhnkndl <jkroyston@gmail.com>
2021-01-28 16:52:23 -0500

    Create and test initial questions array

--- index.js
+++ index.js
@@ -3,13 +3,79 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [];
+const questions = [
+  {
+    name: "start",
+    type: "confirm",
+    message: "In order to generate a quality README, first we need to gather some information. \nAre you ready to begin?"
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "Name:"
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Email Address:"
+  },
+  {
+    name: "github",
+    type: "input",
+    message: "GitHub Username:"
+  },
+  {
+    name: "instructions",
+    type: "confirm",
+    message: "Great. As we proceed forward, some questions may require more detailed responses. \nWe'll open and use your local text editor to capture these responses. \nFor more information, please refer to this application's documentation. \nAre you ready to proceed?"
+  },
+  {
+    name: "title",
+    type: "input",
+    message: "Application Title:"
+  },
+  {
+    name: "description",
+    type: "editor",
+    message: "Describe the application:"
+  },
+  {
+    name: "installation",
+    type: "editor",
+    message: "Enter installation instructions (if applicable):"
+  },
+  {
+    name: "usage",
+    type: "editor",
+    message: "Enter detailed usage instructions:"
+  },
+  {
+    name: "contributing",
+    type: "editor",
+    message: "Enter guidelines for contributions:"
+  },
+  {
+    name: "tests",
+    type: "editor",
+    message: "Enter testing suite details:"
+  },
+  {
+    name: "license",
+    type: "list",
+    choices: [
+      "MIT", "Apache 2.0", "GPL v3", "BSD 3"
+    ],
+    message: "Select a license for your application:"
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
