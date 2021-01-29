fcf24c5cb0753be552f7cacc306c4d159e6a0413
jxhnkndl <jkroyston@gmail.com>
2021-01-29 18:02:21 -0500

    Write functions to generate a license badge and a license link

--- index.js
+++ index.js
@@ -3,72 +3,93 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// Questions array
 const questions = [
   {
-    name: "start",
+    name: "os",
+    type: "list",
+    message: `This application will guide you through generating a README document.
+    Markdown formatting is supported.
+    Before we proceed, are you using a Windows machine or a Mac/Linux machine?`,
+    choices: ["Windows", "Mac/Linux"]
+  },
+  {
+    name: "Windows",
     type: "confirm",
-    message: "In order to generate a quality README, first we need to gather some information. \nAre you ready to begin?"
+    message: `The application will use Notepad for questions that may require longer responses. 
+    Are you ready to get started?`,
+    when: answers => answers.os === "Windows"
+  },
+  {
+    name: "Mac/Linux",
+    type: "confirm",
+    message: `The application will use Vim for questions that may require longer responses. 
+    To use Vim, press "enter" to open the editor and "i" to start typing.
+    When you're finished, press "esc" followed by ":wq" to save your answer and close Vim.
+    Are you ready to get started?`,
+    when: answers => answers.os === "Mac/Linux"
   },
   {
     name: "name",
-    type: "editor",
-    message: "Name:"
+    type: "input",
+    message: `Name:`
   },
   {
     name: "email",
     type: "input",
-    message: "Email Address:"
+    message: `Email Address:`
   },
   {
     name: "github",
     type: "input",
-    message: "GitHub Username:"
+    message: `GitHub Username:`
   },
-  {
-    name: "instructions",
-    type: "confirm",
-    message: "Great. As we proceed forward, some questions may require more detailed responses. \nWe'll open and use your local text editor to capture these responses. \nFor more information, please refer to this application's documentation. \nAre you ready to proceed?"
-  },
+
   {
     name: "title",
     type: "input",
-    message: "Application Title:"
+    message: `Application Title:`
   },
-  {
-    name: "description",
-    type: "editor",
-    message: "Describe the application:"
-  },
-  {
-    name: "installation",
-    type: "input",
-    message: "Enter installation instructions (if applicable):"
-  },
-  {
-    name: "usage",
-    type: "editor",
-    message: "Enter detailed usage instructions:"
-  },
-  {
-    name: "contributing",
-    type: "editor",
-    message: "Enter guidelines for contributions:"
-  },
-  {
-    name: "tests",
-    type: "editor",
-    message: "Enter testing suite details:"
-  },
+  // {
+  //   name: "description",
+  //   type: "editor",
+  //   message: `Describe the application:`
+  // },
+  // {
+  //   name: "installation",
+  //   type: "input",
+  //   message: `How would a user install the application?`
+  // },
+  // {
+  //   name: "usage",
+  //   type: "editor",
+  //   message: `Please provide instructions for how to use the application:`
+  // },
+  // {
+  //   name: "contributing",
+  //   type: "editor",
+  //   message: `What are the guidelines for contributions to the project?`
+  // },
+  // {
+  //   name: "tests",
+  //   type: "editor",
+  //   message: `Please provide information about testing suites and environments:`
+  // },
   {
     name: "license",
     type: "list",
-    choices: [
-      "MIT", "Apache 2.0", "GPL v3", "BSD 3"
-    ],
+    choices: ["MIT", "Apache 2.0", "GPL v3", "GPL v2", "BSD 3", "No License"],
     message: "Select a license for your application:"
+  },
+  {
+    name: "badgeColor",
+    type: "list",
+    choices: ["brightgreen", "yellow", "red", "blue", "orange", "lightgray", "blueviolet"],
+    message: "Select a color for your license badge:"
   }
 ];
 
+// Take the formatted answers and write them into a new or existing file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, err => {
     if (err) new Error(err);
@@ -76,11 +97,12 @@ function writeToFile(fileName, data) {
   });
 }
 
-
+// Init application and begin the command line prompts
+// When answers are returned, use them to generate and write the markdown file
 function init() {
   inquirer.prompt(questions)
     .then(answers => {
-      writeToFile("test.md", generateMarkdown(answers))
+      writeToFile("test.md", generateMarkdown(answers));
     })
     .catch(err => console.log(err));
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,55 +1,102 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license, color) {
+  const badge = `https://img.shields.io/static/v1?style=plastic&label=license&message=${license}&color=${color}`;
+
+  if (license && color) {
+    return badge;
+
+  } else if (license && !color) {
+    color = "blue";
+    return badge;
+
+  } else if (!license) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const links = [
+    { 
+      license: "MIT", 
+      link: "https://opensource.org/licenses/MIT" 
+    },
+    { 
+      license: "Apache 2.0", 
+      link: "https://opensource.org/licenses/Apache-2.0" 
+    },
+    { 
+      license: "GPL v3", 
+      link: "https://www.gnu.org/licenses/gpl-3.0" 
+    },
+    { 
+      license: "GPL v2", 
+      link: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html" 
+    },
+    { 
+      license: "BSD 3", 
+      link: "https://opensource.org/licenses/BSD-3-Clause" 
+    }
+  ];
+
+  const licenseMatch = links.find(link => link.license === license);
+  return licenseMatch.link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // Generate markdown file
-const generateMarkdown = (data) => `
-# ${data.title}
+const generateMarkdown = (data) => {
+  const license = data.license;
+  const color = data.badgeColor;
+  const badge = renderLicenseBadge(license, color);
+  const link = renderLicenseLink(license);
 
-![license](https://img.shields.io/static/v1?label=license&message=MIT&color=blue&style=flat-square)
+  console.log(badge);
+  console.log(link);
 
-## Table of Contents
+  return `# ${data.title}
 
-* [Description](#description)
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-
-# Description
-${data.description}
-
-## Installation
-${data.installation}
-
-## Usage
-${data.usage}
-
-## License
-Copyright (c) 2021 ${data.name}  
-Licensed under the ${data.license} license
-
-## Contributing
-${data.contributing}
-
-## Tests
-${data.tests}
-
-## Questions
-Need support?   
-Reach me on [GitHub](https://www.github.com/${data.github})  
-Reach me via email at <${data.email}>
-`;
+  ![license](https://img.shields.io/static/v1?label=license&message=MIT&color=blue&style=flat-square)
+  
+  ## Table of Contents
+  
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  # Description
+  ${data.description}
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  Copyright (c) 2021 ${data.name}  
+  Licensed under the ${data.license} license
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  Need support?   
+  Reach me on [GitHub](https://www.github.com/${data.github})  
+  Reach me via email at <${data.email}>
+  `;
+}
 
 // Export generateMarkdown() function
 module.exports = generateMarkdown;
